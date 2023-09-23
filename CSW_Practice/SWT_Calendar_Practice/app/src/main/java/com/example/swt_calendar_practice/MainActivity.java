dc169b026b7b823aea9dbc774a261e7aeaea3bbd
Dustin-08 <cjhjik1@gmail.com>
2023-09-23 19:23:49 +0900

    github 연습_안드로이드 스튜디오 default 코드 업로드

---
+++ CSW_Practice/SWT_Calendar_Practice/app/src/main/java/com/example/swt_calendar_practice/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.swt_calendar_practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
